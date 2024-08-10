4e8ea903c05887e29f4623bd6ba36b5ea089e5ca
tumpa <jtumpa@gmail.com>
2024-08-10 15:08:51 +0200

    feat: invoice filter

---
+++ apps/billing/filters/invoice.py
@@ -0,0 +1,19 @@
+import django_filters
+
+from apps.billing.models.invoice import INVOICE_STATUS, Invoice
+from apps.matters.models import Matter
+
+
+class InvoiceFilter(django_filters.FilterSet):
+    matter = django_filters.ModelChoiceFilter(
+        queryset=Matter.objects.filter(invoice__isnull=False).distinct(),
+        empty_label="All",
+    )
+    status = django_filters.ChoiceFilter(choices=INVOICE_STATUS, empty_label="All")
+    date_issued = django_filters.DateFromToRangeFilter(
+        widget=django_filters.widgets.RangeWidget(attrs={"type": "date"})
+    )
+
+    class Meta:
+        model = Invoice
+        fields = ["matter", "status", "date_issued"]
