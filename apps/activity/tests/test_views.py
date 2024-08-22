590e658b2b199a0ad824b8ef05318f8d99929863
tumpa <jtumpa@gmail.com>
2024-08-22 21:20:30 +0200

    fix: unit tests

--- apps/activity/tests/test_views.py
+++ apps/activity/tests/test_views.py
@@ -13,7 +13,7 @@ def test_index(client):
     response = client.get("/activity/")
     assert response.status_code == 200
 
-    response = client.get(reverse("activity:list"))
+    response = client.get(reverse("activity:time-list"))
     assert response.status_code == 200
 
     assertTemplateUsed(response, "activity/list.html")
@@ -22,23 +22,23 @@ def test_index(client):
 
 
 def test_add_get(client):
-    response = client.get("/activity/add")
+    response = client.get("/activity/time/add")
     assert response.status_code == 200
-    assertTemplateUsed(response, "activity/form.html")
+    assertTemplateUsed(response, "activity/time/form.html")
     assert response.context["add"]
 
 
 def test_add_post(client, entry_data):
-    response = client.post(reverse("activity:add"), entry_data)
+    response = client.post(reverse("activity:time-add"), entry_data)
     assert response.status_code == 302
     found = TimeEntry.objects.filter(actions=entry_data["actions"]).first()
     assert found
 
 
 def test_edit_get(client, entry):
-    response = client.get(f"/activity/{entry.id}/edit")
+    response = client.get(f"/activity/time/{entry.id}/edit")
     assert response.status_code == 200
-    assertTemplateUsed(response, "activity/form.html")
+    assertTemplateUsed(response, "activity/time/form.html")
 
 
 def test_edit_post(client, matter, entry):
@@ -49,21 +49,21 @@ def test_edit_post(client, matter, entry):
         "hours": 2.5,
         "rate": 300,
     }
-    response = client.post(f"/activity/{entry.id}/edit", data)
+    response = client.post(f"/activity/time/{entry.id}/edit", data)
     assert response.status_code == 302
     found = TimeEntry.objects.filter(actions="new actions").exists()
     assert found
 
 
 def test_delete(client, entry):
-    response = client.get(f"/activity/{entry.id}/delete")
+    response = client.get(f"/activity/time/{entry.id}/delete")
     assert response.status_code == 302
     found = TimeEntry.objects.filter(pk=entry.id).exists()
     assert not found
 
 
 def test_filter(client):
-    response = client.post("/activity/filter-time-entries/")
+    response = client.post("/activity/time/filter")
     assert response.status_code == 302
     assertTemplateUsed("activity/time-entries-filter.html")
 
@@ -81,29 +81,29 @@ def test_filter_update(client):
         "tab": "time",
         "invoiced": "1",
     }
-    response = client.post("/activity/filter-time-entries/", data)
+    response = client.post("/activity/time/filter", data)
     assert response.status_code == 302
-    response = client.get("/activity/filter-time-entries/")
+    response = client.get("/activity/time/filter")
     assert response.context["form"]["entered"].value() == "0"
 
 
 def test_filter_quick(client):
-    response = client.get("/activity/quick-filter-today/time")
+    response = client.get("/activity/time/filter/quick/today")
     assert response.status_code == 302
-    response = client.get("/activity/filter-time-entries/")
+    response = client.get("/activity/time/filter")
     assert client.session["time_filter"]["date_min"] == date.today().isoformat()
 
 
 def test_filter_matter(client, matter):
-    response = client.get(f"/activity/filter-matter/{matter.id}/time")
+    response = client.get(f"/activity/time/filter/matter/{matter.id}")
     assert response.status_code == 302
-    response = client.get("/activity/filter-time-entries/")
+    response = client.get("/activity/time/filter")
     assert client.session["time_filter"]["matter"] == matter.id
 
 
 def test_toggle_entered(client, entry):
     assert entry.entered == 0
-    client.get(f"/activity/{entry.id}/toggle-entered")
+    client.get(f"/activity/time/{entry.id}/toggle-entered")
     entry.refresh_from_db()
     assert entry.entered == 1
 
