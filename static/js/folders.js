fb7d82a68f5f3a4d51e37ec9161bcceed5feb34c
72hrs <jtumpa@gmail.com>
2024-09-04 21:43:59 +0200

    feat: made contacts the same as minhome

--- static/js/folders.js
+++ static/js/folders.js
@@ -1,58 +1,49 @@
 function showEditFolderForm() {
+  // get the folder list item
+  const parentElement = event.target.parentElement.parentElement.parentElement;
 
-    // get the folder list item
-    const parentElement = event.target.parentElement.parentElement;
+  // hide the folder
+  let child = parentElement.querySelector(".show-folder");
+  child.style.display = "none";
 
-    // hide the folder
-    let child = parentElement.querySelector('.show-folder');
-    child.style.display = 'none';
+  // display the folder edit form and delete icon
+  child = parentElement.querySelector(".edit-folder");
+  child.style.display = "flex";
 
-    // display the folder edit form and delete icon
-    child = parentElement.querySelector('.edit-folder');
-    child.style.display = 'flex';
+  // focus on the edit folder input
+  const input = child.querySelector(".edit-folder-input");
+  input.focus();
 
-    // focus on the edit folder input
-    const input = child.querySelector('.edit-folder-input');
-    input.focus();
-
-    // move the cursor to the end
-    const currentValue = input.value; //store the value of the element
-    input.value = ''; //clear the value of the element
-    input.value = currentValue;
+  // move the cursor to the end
+  const currentValue = input.value; //store the value of the element
+  input.value = ""; //clear the value of the element
+  input.value = currentValue;
 }
-
 
 function hideEditFolderForm() {
+  // get the folder list item
+  const parentElement =
+    event.target.parentElement.parentElement.parentElement.parentElement;
 
-    // get the folder list item
-    const parentElement = event.target.parentElement.parentElement.parentElement;
+  // hide the folder
+  let child = parentElement.querySelector(".show-folder");
+  child.style.display = "inline";
 
-    setTimeout(function () {
-
-        // hide the folder
-        let child = parentElement.querySelector('.show-folder');
-        child.style.display = 'inline';
-
-        // display the folder edit form and delete icon
-        child = parentElement.querySelector('.edit-folder');
-        child.style.display = 'none';
-
-    }, 500);
+  // display the folder edit form and delete icon
+  child = parentElement.querySelector(".edit-folder");
+  child.style.display = "none";
 }
-
 
 function showAddFolderForm() {
-    let addFolderItem = document.querySelector('#add-folder-item');
-    addFolderItem.style.display = 'flex';
-    addFolderItem.querySelector('.edit-folder').style.display = 'flex';
-    addFolderItem.querySelector('#add-folder-input').focus();
+  let addFolderItem = document.querySelector("#add-folder-item");
+  addFolderItem.style.display = "flex";
+  addFolderItem.querySelector(".edit-folder").style.display = "flex";
+  addFolderItem.querySelector("#add-folder-input").focus();
 }
 
-
 function hideAddFolderForm() {
-
-    setTimeout(function () {
-        const elementId = 'add-folder-item';
-        hide(elementId);
-    }, 0);
+  setTimeout(function () {
+    const elementId = "add-folder-item";
+    hide(elementId);
+  }, 0);
 }
