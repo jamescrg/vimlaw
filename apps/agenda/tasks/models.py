4c5d6027abb12fd101fdc03ff78b190707cab3cd
James Craig <jamespcraig@gmail.com>
2024-09-14 13:04:35 +0000

    tasks: rename priority to urgent and update form and admin, make migration

--- apps/agenda/tasks/models.py
+++ apps/agenda/tasks/models.py
@@ -12,7 +12,7 @@ class Task(models.Model):
     date_due = models.DateField(blank=True, null=True)
     matter = models.ForeignKey(Matter, on_delete=models.CASCADE, blank=True, null=True)
     status = models.CharField(max_length=50, null=True)
-    priority = models.BooleanField(default=False)
+    urgent = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.description} : {self.id}"
