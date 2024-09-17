b3cb9a5aa64d7cf29e83176745ed8c92668a8a22
Jan Tumpa <jan@vimlaw.org>
2024-09-17 16:24:26 +0200

    feat: clear filters now works with htmx and without

--- apps/management/urls.py
+++ apps/management/urls.py
@@ -5,5 +5,10 @@ from apps.management.views import clear_filters
 app_name = "management"
 
 urlpatterns = [
+    path(
+        "clear-filters/<str:session_key>/<str:trigger>/",
+        clear_filters,
+        name="clear-filters",
+    ),
     path("clear-filters/<str:session_key>/", clear_filters, name="clear-filters"),
 ]
