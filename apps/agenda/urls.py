49c17bf8e964e4730ca02554d85a2df59e6ec2b4
James Craig <jamespcraig@gmail.com>
2024-09-21 18:13:21 +0000

    removed unnecessary slash from agenda index route

--- apps/agenda/urls.py
+++ apps/agenda/urls.py
@@ -28,7 +28,7 @@ from apps.agenda.tasks.views import (
 app_name = "agenda"
 
 urlpatterns = [
-    path("/", tasks_index, name="tasks-index"),
+    path("", tasks_index, name="tasks-index"),
     path("agenda/tasks", tasks_select, name="tasks-select"),
     path("agenda/tasks/add", tasks_add, name="tasks-add"),
     path("agenda/tasks/<int:id>/edit", tasks_edit, name="tasks-edit"),
