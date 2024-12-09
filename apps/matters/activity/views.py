b7364f5946f36852778338befe6f2675192a2fd4
Jan Tumpa <jtumpa@gmail.com>
2024-12-09 23:02:29 +0100

    feat: full activity report on matter details

--- apps/matters/activity/views.py
+++ apps/matters/activity/views.py
@@ -1,29 +1,59 @@
-from datetime import date, timedelta
-
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, render
 
 from apps.activity.time.models import TimeEntry
+from apps.management.pagination import CustomPaginator
 from apps.matters.models import Matter
 from apps.matters.proceedings.models import Proceeding
 
 
 @login_required
-def index(request, id):
+def activity_index(request, id):
     matter = get_object_or_404(Matter, pk=id)
 
     proceeding = Proceeding.objects.filter(matter=matter.id).order_by("-id").first()
 
-    thirty_days = date.today() - timedelta(days=30)
+    entries = TimeEntry.objects.filter(matter=id).order_by("-id")
 
-    entries = TimeEntry.objects.filter(matter=id, date__gt=thirty_days).order_by("-id")
+    pagination = CustomPaginator(
+        entries, per_page=10, request=request, session_key="activity_pagination"
+    )
 
     context = {
         "app": "matters",
         "subapp": "activity",
         "matter": matter,
         "proceeding": proceeding,
-        "entries": entries,
+        "entries": pagination.get_object_list(),
+        "pagination": pagination,
+        "session_key": "activity_pagination",
+        "trigger_key": "matterActivityChanged",
+    }
+
+    return render(request, "matters/activity/main.html", context)
+
+
+@login_required
+def activity_list(request, id):
+    matter = get_object_or_404(Matter, pk=id)
+
+    proceeding = Proceeding.objects.filter(matter=matter.id).order_by("-id").first()
+
+    entries = TimeEntry.objects.filter(matter=id).order_by("-id")
+
+    pagination = CustomPaginator(
+        entries, per_page=1, request=request, session_key="activity_pagination"
+    )
+
+    context = {
+        "app": "matters",
+        "subapp": "activity",
+        "matter": matter,
+        "proceeding": proceeding,
+        "entries": pagination.get_object_list(),
+        "pagination": pagination,
+        "session_key": "activity_pagination",
+        "trigger_key": "matterActivityChanged",
     }
 
     return render(request, "matters/activity/list.html", context)
