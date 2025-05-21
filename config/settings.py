74ded610967bdb54b09c6992559b2a1beee6f112
James Craig <jamespcraig@gmail.com>
2025-05-21 21:23:23 +0000

    agenda: tasks: removed django-unfold

--- config/settings.py
+++ config/settings.py
@@ -45,7 +45,6 @@ ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 # Application definition
 
 INSTALLED_APPS = [
-    "unfold",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
