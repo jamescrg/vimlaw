684e3f2bf4be0229959c26d1db3f1fb07545c776
James Craig <jamespcraig@gmail.com>
2025-08-14 19:33:27 +0000

    matters: ledger: update pdf template to include trust balance and current date, all fonts Noto Serif

--- apps/matters/ledger/generate_ledger.py
+++ apps/matters/ledger/generate_ledger.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from tempfile import NamedTemporaryFile
 
 from django.http import Http404
@@ -7,6 +8,7 @@ from weasyprint import HTML
 from apps.matters.ledger.get_ledger_data import get_ledger_data
 from apps.matters.models import Matter
 from apps.matters.proceedings.models import Proceeding
+from apps.trust.trust import get_confirmed_client_balance
 
 
 def generate_ledger(matter_id, request):
@@ -18,15 +20,24 @@ def generate_ledger(matter_id, request):
     except Matter.DoesNotExist:
         raise Http404("Matter does not exist")
 
+    current_date = datetime.now().strftime("%Y-%m-%d")
+
     ledger_data = get_ledger_data(matter)
     proceeding = Proceeding.objects.filter(matter=matter.id).order_by("-id").first()
+
+    # Get client trust balance
+    client_trust_balance = 0
+    if matter.client:
+        client_trust_balance = get_confirmed_client_balance(matter.client.id)
 
     context = {
         "matter": matter,
         "proceeding": proceeding,
+        "current_date": current_date,
+        "client_trust_balance": client_trust_balance,
     } | ledger_data
 
-    html_string = render_to_string("matters/ledger/ledger.html", context)
+    html_string = render_to_string("matters/ledger/pdf.html", context)
     html = HTML(string=html_string, base_url=request.build_absolute_uri())
 
     with NamedTemporaryFile(suffix=".pdf", delete=False) as pdf_file:
