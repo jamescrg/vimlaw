51c7b64642210effa6926b114849327fd18e603d
James Craig <james@vimlaw.org>
2025-08-14 14:40:08 +0000

    matters: ledger: add credit detail to description column

--- apps/matters/ledger/get_ledger_data.py
+++ apps/matters/ledger/get_ledger_data.py
@@ -52,7 +52,7 @@ def get_ledger_data(matter):
                 "id": credit.id,
                 "date": credit.date,
                 "transaction_type": "Credit",
-                "description": f"Credit {credit.id}",
+                "detail": credit.detail,
                 "amount": credit.amount,
                 "affects_balance": True,  # Credits always affect balance
             }
