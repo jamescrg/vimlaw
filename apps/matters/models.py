3acfe4f392d78b92a8329c3dca5ab6fde33f6363
James Craig <james@vimlaw.org>
2025-11-18 15:51:42 +0000

    reports: statement: add matter summary table

--- apps/matters/models.py
+++ apps/matters/models.py
@@ -7,6 +7,7 @@ class Matter(models.Model):
     user_id = models.IntegerField(null=True)
     name = models.CharField(max_length=50, null=True)
     work_status = models.CharField(max_length=255, null=True)
+    description = models.CharField(max_length=255, null=True)
     status = models.CharField(max_length=50, null=True)
     date_start = models.DateField(null=True)
     date_end = models.DateField(blank=True, null=True)
