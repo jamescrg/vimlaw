65fcdee7555b5333e377a559a802c0f0f126bd05
James Craig <james@vimlaw.org>
2025-12-09 19:43:11 +0000

    documents - edits to scroll behavior, forms

--- apps/case/highlights/forms.py
+++ apps/case/highlights/forms.py
@@ -10,12 +10,14 @@ IMPORTANCE_CHOICES = tuple((i, f"Importance {i}") for i in range(1, 11))
 class HighlightForm(forms.ModelForm):
     class Meta:
         model = Highlight
-        fields = ["color", "paragraph_number", "importance", "slug", "text"]
+        fields = ["importance", "color", "paragraph_number", "slug", "text"]
         widgets = {
             "color": forms.Select(),
             "importance": forms.Select(choices=IMPORTANCE_CHOICES),
             "paragraph_number": forms.TextInput(),
-            "slug": forms.TextInput(attrs={"class": "span3", "required": True}),
+            "slug": forms.TextInput(
+                attrs={"class": "span3", "required": True, "autofocus": True}
+            ),
             "text": forms.Textarea(attrs={"class": "span3", "rows": 8}),
         }
 
